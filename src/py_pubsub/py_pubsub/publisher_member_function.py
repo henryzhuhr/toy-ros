e4ebc4232327b84a59b66a1056eba0663102e39f
zmy <1476977013@qq.com>
2025-01-10 18:57:43 +0800

    添加创建包的学习记录，看懂代码做了注释

--- src/py_pubsub/py_pubsub/publisher_member_function.py
+++ src/py_pubsub/py_pubsub/publisher_member_function.py
@@ -1,39 +1,39 @@
 import rclpy
-from rclpy.node import Node
+from rclpy.node import Node     # 导入Node类
 
-from std_msgs.msg import String
+from std_msgs.msg import String     # 导入String消息类型
 
 
-class MinimalPublisher(Node):
+class MinimalPublisher(Node):    # 创建一个继承自Node的类
 
-    def __init__(self):
-        super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+    def __init__(self):        # 构造函数，在类实例化时会自动调用
+        super().__init__('minimal_publisher')     # 调用父类的构造
+        self.publisher_ = self.create_publisher(String, 'topic', 10)    # 创建一个发布者,将数据发布到名为 'topic' 的主题，消息类型是 String 10是发布队列的长度
+        timer_period = 0.5  # 单位seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)   # 创建一个定时器，每隔0.5秒调用一次timer_callback 
         self.i = 0
 
-    def timer_callback(self):
+    def timer_callback(self):   # 定时器回调函数
         msg = String()
         msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.publisher_.publish(msg)   # 发布消息
+        self.get_logger().info('Publishing: "%s"' % msg.data)    # 打印日志
         self.i += 1
 
 
 def main(args=None):
-    rclpy.init(args=args)
+    rclpy.init(args=args)  # 初始化ROS2
 
-    minimal_publisher = MinimalPublisher()
+    minimal_publisher = MinimalPublisher()   # 创建节点实例
 
-    rclpy.spin(minimal_publisher)
+    rclpy.spin(minimal_publisher)   # 进入spin循环
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
-    rclpy.shutdown()
+    minimal_publisher.destroy_node()    # 销毁节点
+    rclpy.shutdown()    # 关闭ROS2
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':   #用于判断当前脚本是否作为主程序执行。如果是，则执行main()函数
     main()
--- src/py_pubsub/py_pubsub/subscriber_member_function.py
+++ src/py_pubsub/py_pubsub/subscriber_member_function.py
@@ -1,30 +1,30 @@
 import rclpy
-from rclpy.node import Node
+from rclpy.node import Node  # 导入Node类
 
-from std_msgs.msg import String
+from std_msgs.msg import String  # 导入String消息类型
 
 
-class MinimalSubscriber(Node):
+class MinimalSubscriber(Node):     # 创建一个继承自Node的类
 
-    def __init__(self):
-        super().__init__('minimal_subscriber')
-        self.subscription = self.create_subscription(
+    def __init__(self):          # 构造函数，在类实例化时会自动调用
+        super().__init__('minimal_subscriber')         # 调用父类的构造 
+        self.subscription = self.create_subscription(      # 创建一个订阅者
             String,
             'topic',
-            self.listener_callback,
-            10)
-        self.subscription  # prevent unused variable warning
+            self.listener_callback,      # 回调函数
+            10)             # 订阅队列长度为10
+        self.subscription  # prevent unused variable warning 预防未使用参数警告
 
     def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
+        self.get_logger().info('I heard: "%s"' % msg.data)          # 打印日志
 
 
 def main(args=None):
-    rclpy.init(args=args)
+    rclpy.init(args=args)               # 初始化ROS2    
 
-    minimal_subscriber = MinimalSubscriber()
+    minimal_subscriber = MinimalSubscriber()            # 创建节点实例                    
 
-    rclpy.spin(minimal_subscriber)
+    rclpy.spin(minimal_subscriber)                  # 进入spin循环
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
